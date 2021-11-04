8d69cb22542f3fa8d0515fef773534a49c7ae427
mani-kv <mani92kv@gmail.com>
2021-11-03 21:12:05 -0700

    updates

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,15 @@
 /** @type {import('@sveltejs/kit').Config} */
 
 import {markdown} from 'svelte-preprocess-markdown'
+import adapter from '@sveltejs/adapter-netlify';
+
 
 const config = {
 	extensions: ['.svelte','.md'],
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: adapter()
 	},
 	preprocess: markdown()
 };
