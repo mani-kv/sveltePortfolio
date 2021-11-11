a52c00800b9216fe35a76389bff21c3abb9ef9d4
mani-kv <mani92kv@gmail.com>
2021-11-10 18:36:43 -0800

    updated 404 page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,11 @@
+<svelte:head>
+    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+</svelte:head>
+
+<div class="flex flex-col items-center">
+    <span class="text-xl font-light">
+        Pushing Pixels, Comeback later :)
+    </span>
+    <lottie-player src="/lottie-404-page.json"  background="transparent"  speed="1"  loop  autoplay></lottie-player>
+</div>
+
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -9,7 +9,7 @@ import SvelteSeo from "svelte-seo";
   keywords="key1, key2, key3"
 />
 
-<div class="">
+<div class="flex">
 
     Test
 </div>
